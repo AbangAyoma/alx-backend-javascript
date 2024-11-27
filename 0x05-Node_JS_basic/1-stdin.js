50082f5053a35a8a78f48d8e05f4101faebb09d2
AbangAyoma <abangayoma@gmail.com>
2024-11-27 14:32:57 +0100

    command line args

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,9 +3,20 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
-  process.exit();
+//   process.exit();
 });
 
 process.on('exit', () => {
   console.log('This important software is now closing');
 });
+
+// process.stdout.write("welcome onboard\ntype in something\n");
+// process.stdin.on('data', (data)=>{
+//     const fortm = data.toString().trim();
+    // console.log(`Your name is: ${dem}`);
+    // process.stdout.write(`Your name is: ${ fortm }\n`);
+    // process.exit();
+// });
+// process.on('exit', ()=>{
+    // console.log("you're leaving this great app");
+// })
